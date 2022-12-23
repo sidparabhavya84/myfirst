1f903d12a5717d05fd7b9b787cff2d5e01c2b2ab
sidparabhavya84 <sidparabhavya84@gmail.com>
2022-12-23 11:42:15 +0530

    TODO

---
+++ src/componants/ToDo.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+// import ToHeader from "./ToHeader";
+import './Todo.css'
+
+
+function Todo(){
+    const [todos,setTodos] = useState([])
+    const [todo,setTodo] = useState("")
+    const [todoEditing,setTodoEditing] = useState(null)
+
+    function handleSubmit(e){
+        e.preventDefault()
+
+        const newTodo ={
+            id: new Date().getTime(),
+            text: todo,
+            completed:false,
+        }
+        setTodos([...todos].concat(newTodo))
+        setTodo("")
+    }
+
+    function deleteTodo(id) {
+        const updatedTodos = [... todos].filter((todo) => todo.id !== id)
+        setTodos(updatedTodos)
+    }
+
+    function toggleComplete(id) {
+        const updatedTodos = [...todos].map((todo) => {
+            if (todo.id === id){
+                todo.completed = !todo.completed
+            }
+            return todo
+        })    
+        setTodos(updatedTodos)
+    }
+
+    return(
+        <div className="App">
+            <form onSubmit={handleSubmit}>
+                <h2>Add Item... </h2>
+                <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo} name="name"/>
+                <br/>
+                <br/>
+                <button type="submit" className="sub">Add Todo</button>
+            </form>
+            {todos.map((todo) => <div key={todo.id}>
+                <div className="ans">{todo.text}</div> 
+                <button onClick={() => deleteTodo(todo.id)} className="del">Delete</button>
+                
+            </div>)}
+            <button className="clear">CLEAR ALL</button>
+        </div>
+    );
+
+}
+
+
+export default Todo;
--- src/index.js
+++ src/index.js
@@ -2,22 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 // import App from './componants/App';
-import reportWebVitals from './reportWebVitals';  
 // import Gallary from './componants/Gallary';
 // import title from './componants/title';
 // import Header from './componants/Header';
-import Counter from './componants/Counter';
+// import Counter from './componants/Counter';
+// import HoverCounter from './componants/HoverCounter';
 // import Theamchanger from './componants/Theamchanger';
-
+// import CounterFun from './componants/CounterFun';
+// import PageWidth from './componants/PageWidth';
+// import ClickCounter from './componants/ClickCounter';
+// import Tab from './componants/Tab';
+// import Key from './componants/Key';
+// import FormHandlar from './componants/FormHandlar';
+// import ManageForm from './componants/ManageForm';
+// import FHandlar from './componants/FHandlar';
+import ToDO from './componants/ToDo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // const list = ["Home","About us","Blog","Services","Contact us","Help"];
 
 root.render(
   <>
-    <Counter />
+    {/* <Key/> */}
+    {/* <Tab/> */}
+     {/* <PageWidth/> */}
+     {/* <ClickCounter/> */}
+    {/* <Counter /> */}
+    {/* <HoverCounter/> */}
     {/* <Theamchanger /> */}
     {/* <Header logo = 'images/logo.png' list = {list}/> */}
+    {/* <CounterFun/>  */}
+    {/* <ClickCounter /> */}
+    {/* <FormHandlar/> */}
+    {/* <ManageForm/> */}
+    {/* <FHandlar/> */}
+    <ToDO />
+
+   
     
     
     
