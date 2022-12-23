d61ec04399e58e4dd4a7f34f77dedda99b8a14bf
sidparabhavya84 <sidparabhavya84@gmail.com>
2022-12-23 12:07:20 +0530

    TODO

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
-// import App from './componants/App';
-// import Gallary from './componants/Gallary';
-// import title from './componants/title';
-// import Header from './componants/Header';
-// import Counter from './componants/Counter';
-// import HoverCounter from './componants/HoverCounter';
-// import Theamchanger from './componants/Theamchanger';
-// import CounterFun from './componants/CounterFun';
-// import PageWidth from './componants/PageWidth';
-// import ClickCounter from './componants/ClickCounter';
-// import Tab from './componants/Tab';
-// import Key from './componants/Key';
-// import FormHandlar from './componants/FormHandlar';
-// import ManageForm from './componants/ManageForm';
-// import FHandlar from './componants/FHandlar';
+
 import ToDO from './componants/ToDo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -23,25 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
-    {/* <Key/> */}
-    {/* <Tab/> */}
-     {/* <PageWidth/> */}
-     {/* <ClickCounter/> */}
-    {/* <Counter /> */}
-    {/* <HoverCounter/> */}
-    {/* <Theamchanger /> */}
-    {/* <Header logo = 'images/logo.png' list = {list}/> */}
-    {/* <CounterFun/>  */}
-    {/* <ClickCounter /> */}
-    {/* <FormHandlar/> */}
-    {/* <ManageForm/> */}
-    {/* <FHandlar/> */}
-    <ToDO />
-
-   
-    
-    
-    
+    <ToDO />    
   </>
 );
 
